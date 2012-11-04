15e3a4760ba1e5e0ebfafc952891328dae7684fb
Elco Jacobs <github@brewpi.com>
2012-11-04 22:31:18 +0100

    Fixed typo in prepareJSON in index.php

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ function prepareJSON($input) {
     //This will convert ASCII/ISO-8859-1 to UTF-8.
     //Be careful with the third parameter (encoding detect list), because
     //if set wrong, some input encodings will get garbled (including UTF-8!)
-    $imput = mb_convert_encoding($input, 'UTF-8', 'ASCII,UTF-8,ISO-8859-1');
+    $input = mb_convert_encoding($input, 'UTF-8', 'ASCII,UTF-8,ISO-8859-1');
 
     //Remove UTF-8 BOM if present, json_decode() does not like it.
     if(substr($input, 0, 3) == pack("CCC", 0xEF, 0xBB, 0xBF)) $input = substr($input, 3);
