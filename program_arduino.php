8cdf2a436fdd0af9b612861714e3c78632441a31
Matthew McGowan <mdm@techie.com>
2013-02-19 05:02:02 +0100

    fix for issue 7 - use regular inet sockets on windows
    At present, the switch to the differnet socket type is done manually in
    the config.php file by setting useInetSocket=true.

--- program_arduino.php
+++ program_arduino.php
@@ -69,7 +69,8 @@ else{
 <?php
 $sock = open_socket();
 if($sock !== false){
-	socket_write($sock, "programArduino={\"boardType\":\"$boardType\",\"fileName\":\"$fileName\",\"eraseEEPROM\":$eraseEEPROM}", 1024);
+	$command = "programArduino={\"boardType\":\"$boardType\",\"fileName\":\"$fileName\",\"eraseEEPROM\":$eraseEEPROM}";
+	socket_write($sock, $command, 1024);
 	$avrdudeOutput = socket_read($sock, 4096);
 	socket_close($sock);
 }
@@ -85,22 +86,28 @@ if($sock !== false){
 </html>
 <?php
 function open_socket()
-{
-	$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
-	if ($sock === false) {
-		return false;
-	}
-	else{
-		if(socket_connect($sock, "$GLOBALS[scriptPath]/BEERSOCKET")){
+{	
+	global $useInetSocket;
+	if ($useInetSocket)
+		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	else		
+		$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
+	if (!($sock === false)) 
+	{		
+		if(
+			((!$useInetSocket) && socket_connect($sock, "$GLOBALS[scriptPath]/BEERSOCKET"))
+		     || (($useInetSocket) && socket_connect($sock, $GLOBALS["scriptAddress"], $GLOBALS["scriptPort"])))
+		{
 			socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 15, 'usec' => 0));
-			return $sock;
 		}
-		else{
+		else {
+			socket_close($sock);
 			echo "Not connected";
-			# echo socket_strerror(socket_last_error($sock));
-			return false;
+			if ($GLOBALS->debug===true) 
+				echo socket_strerror(socket_last_error($sock));			
 		}
 	}
+	return $sock;
 }
 ?>
 
