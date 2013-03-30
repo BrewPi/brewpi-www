3aaa1aa1b72f3b6ab07e9047bf98ffe5e6c8adf4
m-mcgowan <mat.mcgowan+github@gmail.com>
2013-03-30 19:38:04 +0100

    added windows socket in socketmessage.php. Should factor out the socket function.

--- socketmessage.php
+++ socketmessage.php
@@ -135,23 +135,27 @@ if($sock !== false){
 }
 
 function open_socket()
-{
-	$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
-	if ($sock === false) {
-		return false;
-	}
-	else{
-		if(socket_connect($sock, "$GLOBALS[scriptPath]/BEERSOCKET")){
+{	
+	global $useInetSocket;
+	if ($useInetSocket)
+		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+	else		
+		$sock = socket_create(AF_UNIX, SOCK_STREAM, 0);
+	if (!($sock === false)) 
+	{		
+		if(
+			((!$useInetSocket) && socket_connect($sock, "$GLOBALS[scriptPath]/BEERSOCKET"))
+		     || (($useInetSocket) && socket_connect($sock, $GLOBALS["scriptAddress"], $GLOBALS["scriptPort"])))
+		{
 			socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 15, 'usec' => 0));
-			return $sock;
 		}
-		else{
-			// will end up on the LCD, don't print the error.
+		else {
+			socket_close($sock);
 			echo "Not connected";
-			// when debugging, uncomment this:
-			// echo socket_strerror(socket_last_error($sock));
-			return false;
+			if ($GLOBALS->debug===true) 
+				echo socket_strerror(socket_last_error($sock));			
 		}
 	}
+	return $sock;
 }
 ?>
