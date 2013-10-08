bbaebb27c1d8e2c06c7635501fde990badba5cba
Elco Jacobs <elcojacobs@gmail.com>
2013-10-08 15:53:20 +0200

    Started with dialog for switching beer name/active brew

--- js/main.js
+++ js/main.js
@@ -206,12 +206,90 @@ function checkScriptStatus(){
 		window.setTimeout(refreshLcd, 5000); //alternate refreshing script and lcd
 	});
 }
+
+function beerNameDialogInit(){
+    "use strict";
+    var $dialog = $("<div class='beer-name-dialog'></div>").dialog( {
+        modal: true,
+        title: "Switch active brew",
+        width: 480
+    });
+    var $backButton = $("<button class='back' title='Go back'></button>").button({icons: {primary: "ui-icon-arrowthick-1-w"}, text: false });
+    $dialog.append($backButton);
+    var $body = $("<div class='dialog-body'></div>");
+    $dialog.append($body);
+    beerNameDialogStart($body, $backButton);
+}
+
+function beerNameDialogStart($body, $backButton){
+    "use strict";
+    $body.empty();
+    $backButton.hide();
+    $body.append($("<span  class='dialog-intro'>You are currently fermenting '" + $("#beer-name").text() + "'.<br>What would you like to do?</span>"));
+    var $buttons = $("<div class='beer-name-buttons'></div>");
+    $buttons.append($("<button>Start new brew</button>").button({icons: {primary: "ui-icon-plus"} }).click(function(){
+        beerNameDialogNew($body, $backButton);
+    }));
+    $buttons.append($("<button>Stop this brew</button>").button({icons: {primary: "ui-icon-stop"} }).click(function(){
+        beerNameDialogStop($body, $backButton);
+    }));
+    /*
+    $buttons.append($("<button>Continue existing brew</button>").button({icons: {primary: "ui-icon-folder-open"} }).click(function(){
+
+    }));*/
+    $buttons.append($("<button>Pause logging</button>").button({icons: {primary: "ui-icon-pause"} }).click(function(){
+        beerNameDialogPause($body, $backButton);
+    }));
+    $buttons.find("button").click(function(){
+        $backButton.show().unbind().bind({click: function(){beerNameDialogStart($body, $backButton);}});
+    });
+
+    $body.append($buttons);
+}
+
+function beerNameDialogNew($body, $backButton){
+    "use strict";
+    $body.empty();
+    $body.append($("<span  class='dialog-intro'>Please enter a name for your new brew. Your current brew will be stopped and BrewPi will start logging data for your new brew.</span>"));
+    $body.append($("<input id='new-beer-name' type='text' size='30' placeholder='Enter new beer name..'> </input>"));
+    var $buttons = $("<div class='beer-name-buttons'></div>");
+    $buttons.append($("<button>Start new brew</button>").button({	icons: {primary: "ui-icon-check" } }).click(function(){
+        $.post('socketmessage.php', {messageType: "name", message: $("input#new-beer-name").val()});
+    }));
+    $body.append($buttons);
+}
+
+function beerNameDialogStop($body, $backButton){
+    "use strict";
+    $body.empty();
+    $body.append($("<span  class='dialog-intro'>Clicking stop will finish your current brew and will stop logging data. You can use this when you are between brews.</span>"));
+
+    var $buttons = $("<div class='beer-name-buttons'></div>");
+    $buttons.append($("<button>Stop this brew</button>").button({	icons: {primary: "ui-icon-stop" } }).click(function(){
+        $.post('socketmessage.php', {messageType: "stoplogging", message: ""});
+    }));
+    $body.append($buttons);
+}
+
+function beerNameDialogPause($body, $backButton){
+    "use strict";
+    $body.empty();
+    $body.append($("<span  class='dialog-intro'>Clicking pause will temporarily disable data logging for this brew. You can later continue logging data for the same brew.</span>"));
+
+    var $buttons = $("<div class='beer-name-buttons'></div>");
+    $buttons.append($("<button>Pause logging temporarily</button>").button({	icons: {primary: "ui-icon-pause" } }).click(function(){
+        $.post('socketmessage.php', {messageType: "pauselogging", message: ""});
+    }));
+    $body.append($buttons);
+}
+
 google.load('visualization', '1', {packages: ['table']});
 
 $(document).ready(function(){
 	"use strict";
 	$(".script-status").button({	icons: {primary: "ui-icon-alert" } });
 	$(".script-status span.ui-button-text").text("Checking script..");
+    $("#beer-name").click(beerNameDialogInit);
 
 	loadControlPanel();
 	drawBeerChart(window.beerName, 'curr-beer-chart');
